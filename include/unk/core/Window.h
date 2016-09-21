2b39a21572e652da34dba555a107a5ca2ddfddf6
pedr0rocha <pedr0_r@hotmail.com>
2016-09-21 19:48:27 -0300

    Reworking Window functions

--- include/unk/core/Window.h
+++ include/unk/core/Window.h
@@ -5,24 +5,41 @@
 
 #include "SDL2/SDL.h"
 
+#include <string>
+#include <vector>
+
 namespace unk {
+
+
+	enum class WindowFlags { OPENGL, BORDERLESS, RESIZABLE };
+
 	class Window {
 		public:
-			char* Title;
+			std::string Title;
 			int X;
 			int Y;
 			int Width;
 			int Height;
-			Uint32 Flags;
 			SDL_Window* SDLWindow;
 
-			Window(char* title, int x, int y, int width, int height, Uint32 flags);
-			SDL_Renderer* CreateRenderer(int index, Uint32 flags);
-			int SetWindowFullscreen();
-			void MinimizeWindow();
-			void MaximizeWindow();
-			void RestoreWindow();
-			void DestroyWindow();
+			Window(std::string title, int x, int y, int width, int height, std::vector<WindowFlags> flags);
+			Window(std::string title);
+			Window(std::string title, int width, int height);
+
+			~Window();
+			
+			void initWindow(std::string title, int x, int y, int width, int height, 
+				std::vector<WindowFlags> flags);
+
+			SDL_Renderer* createRenderer(int index, uint32_t flags);
+			void setWindowFullscreen();
+			void setWindowFakeFullscreen();
+			void minimizeWindow();
+			void maximizeWindow();
+			void restoreWindow();
+
+		private:
+			uint32_t toSDLFlags(std::vector<WindowFlags> flags);
 	};
 }
 
--- lib/core/Window.cpp
+++ lib/core/Window.cpp
@@ -2,37 +2,72 @@
 
 #include "unk/core/Window.h"
 
-unk::Window::Window(char* title, int x, int y, int width, int height, Uint32 flags) : 
-					Title(title), X(x), Y(y), Width(width), Height(height), Flags(flags) {
+unk::Window::Window(std::string title, int x, int y, int width, int height, 
+	std::vector<WindowFlags> flags) : Title(title), X(x), Y(y), Width(width), Height(height) {
+	
+	unk::Window::initWindow(title, x, y, width, height, flags);
+}
 
+unk::Window::Window(std::string title) : Title(title) {
+    unk::Window::initWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, 0, {});
+}
+
+unk::Window::Window(std::string title, int width, int height) : Title(title), 
+	Width(width), Height(height) {
+
+    unk::Window::initWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
+    	width, height, {});
+}
+
+unk::Window::~Window() {
+	SDL_DestroyWindow(SDLWindow);
+}
+
+void unk::Window::initWindow(std::string title, int x, int y, int width, int height, 
+	std::vector<WindowFlags> flags) {
+	
 	SDL_Window *sdlWindow;
-	SDL_Init(SDL_INIT_VIDEO);
 
-	sdlWindow = SDL_CreateWindow(title, x, y, width, height, flags);
+	uint32_t sdlFlags = unk::Window::toSDLFlags(flags);
+	
+	sdlWindow = SDL_CreateWindow(title.c_str(), x, y, width, height, sdlFlags);
 	SDLWindow = sdlWindow;
 }
 
-SDL_Renderer* unk::Window::CreateRenderer(int index, Uint32 flags) {
-	SDL_Renderer* renderer = SDL_CreateRenderer(SDLWindow, index, flags);
+SDL_Renderer* unk::Window::createRenderer(int index, uint32_t flags) {
+	SDL_Renderer *renderer = SDL_CreateRenderer(SDLWindow, index, flags);
 	return renderer;
 }
 
-int unk::Window::SetWindowFullscreen() {
-	return SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN);
+uint32_t unk::Window::toSDLFlags(std::vector<WindowFlags> flags) {
+	uint32_t sdlFlags = 0;
+	for (auto flag : flags) {
+		if (flag == WindowFlags::OPENGL) 		  
+			sdlFlags |= SDL_WINDOW_OPENGL;
+		else if (flag == WindowFlags::BORDERLESS) 
+			sdlFlags |= SDL_WINDOW_BORDERLESS;
+		else if (flag == WindowFlags::RESIZABLE)  
+			sdlFlags |= SDL_WINDOW_RESIZABLE;
+	}
+	return sdlFlags;
 }
 
-void unk::Window::MinimizeWindow() {
+void unk::Window::setWindowFullscreen() {
+	SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN);
+}
+
+void unk::Window::setWindowFakeFullscreen() {
+	SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
+}
+
+void unk::Window::minimizeWindow() {
 	SDL_MinimizeWindow(SDLWindow);
 }
 
-void unk::Window::MaximizeWindow() {
+void unk::Window::maximizeWindow() {
 	SDL_MaximizeWindow(SDLWindow);
 }
 
-void unk::Window::RestoreWindow() {
+void unk::Window::restoreWindow() {
 	SDL_RestoreWindow(SDLWindow);
-}
-
-void unk::Window::DestroyWindow() {
-	SDL_DestroyWindow(SDLWindow);
 }
