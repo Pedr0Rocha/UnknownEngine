529ebdeb0b46edbf7d013d49866783ffb283aba2
Yukio Siraichi <sir.yukio@gmail.com>
2016-09-26 17:52:29 -0300

    Doxygen comments added for Resources and TextureInfo class.

--- include/unk/core/Resources.h
+++ include/unk/core/Resources.h
@@ -13,6 +13,15 @@
 #include <memory>
 
 namespace unk {
+    /**
+     * @brief Class responsible for managing the resources of the program.
+     *
+     * @details It loads an @c SDL_Texture for each different file name. As it manages
+     * all resources, it is treated as a singleton class.
+     *
+     * The @c TextureInfo class is responsible for calling the @c registerTexture function
+     * in order to register the texture, and to be loaded when @c loadAllTextures is called.
+     */
     class Resources {
         private:
             std::map<TextureInfo, SDL_Texture*> Map;
@@ -20,14 +29,21 @@ namespace unk {
             Resources();
 
         public:
+            /// @brief Gets a singleton instance of the @c Resources class.
             static Resources& getResources();
 
+            /// @brief Returns true if a texture with a specific name has already been registred.
             bool hasTexture(TextureInfo info);
 
+            /// @brief Loads all registered textures.
             void loadAllTextures(std::shared_ptr<Renderer> renderer);
+            /// @brief Loads a texture if it has not already been loaded.
             void loadTexture(TextureInfo info, std::shared_ptr<Renderer> renderer);
+            /// @brief Register a texture if it has not already been.
             void registerTexture(TextureInfo info);
+            /// @brief Yields the @c width and @c height of the texture.
             void getTextureMeasures(TextureInfo info, int *width, int *height);
+            /// @brief Returns the @c SDL_Texture related to @c info.
             SDL_Texture *getTexture(TextureInfo info);
     };
 }
--- include/unk/core/TextureInfo.h
+++ include/unk/core/TextureInfo.h
@@ -6,6 +6,12 @@
 #include <string>
 
 namespace unk {
+    /**
+     * @brief Represents a texture with name @c Name.
+     *
+     * @details It has the information of a texture, such as its @ Name and
+     * its dimension (@c Width and @c Height).
+     */
     class TextureInfo {
         private:
             std::string Name;
@@ -13,10 +19,30 @@ namespace unk {
             int Height;
 
         public:
+            /// @brief As soon as it is instantiated, it tries to register the texture
+            /// by calling the @c Resources class function.
             TextureInfo(std::string name);
 
             std::string getName() const;
+
+            /**
+             * @brief Gets the @c width of the texture. 
+             *
+             * @details By the time this class is instantiated, both @c Width and @c Height
+             * are initialized with zero. However, after the call of the loading operation
+             * when this function is called, it retrieves both the dimension by calling the
+             * function @c getMeasures from @c Resources class.
+             */
             int getWidth();
+
+            /**
+             * @brief Gets the @c width of the texture. 
+             *
+             * @details By the time this class is instantiated, both @c Width and @c Height
+             * are initialized with zero. However, after the call of the loading operation
+             * when this function is called, it retrieves both the dimension by calling the
+             * function @c getMeasures from @c Resources class.
+             */
             int getHeight();
 
         private:
