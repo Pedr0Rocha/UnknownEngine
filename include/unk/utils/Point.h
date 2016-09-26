a62a3479f6a350a2097de0ced1f9b080fd071fb4
Yukio Siraichi <sir.yukio@gmail.com>
2016-09-26 02:08:34 -0300

    Point class implemented.

---
+++ include/unk/utils/Point.h
@@ -0,0 +1,24 @@
+/* Unknown Engine Project */
+
+#ifndef UNK_POINT_H
+#define UNK_POINT_H
+
+#include "SDL2/SDL_rect.h"
+
+namespace unk {
+    /**
+     * @brief An abstracted ordered pair.
+     */
+	class Point {
+        public:
+            int X;
+            int Y;
+
+            Point();
+            Point(int x, int y);
+
+            SDL_Point toSDLPoint();
+	};
+}
+
+#endif
