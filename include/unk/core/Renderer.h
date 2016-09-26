161d6b80451e345eaf6b41fee38194f8871de1f6
Yukio Siraichi <sir.yukio@gmail.com>
2016-09-26 03:16:01 -0300

    Texture rendering member function added to Renderer class.

--- include/unk/core/Renderer.h
+++ include/unk/core/Renderer.h
@@ -3,17 +3,107 @@
 #ifndef UNK_RENDERER_H
 #define UNK_RENDERER_H
 
+#include "unk/core/TextureInfo.h"
+#include "unk/utils/Point.h"
+#include "unk/utils/Rect.h"
+
+#include <vector>
+
 class SDL_Renderer;
 
 namespace unk {
 
+    /**
+     * @brief Wraps the SDL equivalent (@c SDL_Renderer).
+     *
+     * @details Is responsible for every function implemented
+     * with the @c SDL_Renderer.
+     */
     class Renderer {
         private:
             SDL_Renderer *SDLRenderer;
+            SDL_RendererInfo Info;
 
         public:
+            /**
+             * @brief The @c SDL_RendererFlags.
+             */
+            enum class Flags {
+                SOFTWARE,       ///< Software fallback.
+                ACCELERATED,    ///< Uses hardware acceleration.
+                VSYNC,          ///< Synchronizes the refresh rate.
+                TEXTURE         ///< Render to texture.
+            };
+
+            /**
+             * @brief Indicates whether the @c Renderer should flip the
+             * image or not.
+             */
+            enum class Flip {
+                NONE,       ///< No flip.
+                HORIZONTAL, ///< Flip horizontally.
+                VERTICAL    ///< Flip vertically.
+            };
+
+            /// @brief Creates a @c Renderer with a @c Window and standard 
+            /// flags (ACCELERATED).
             Renderer();
+            /// @brief Creates a customized @c Renderer with custom flags.
+            Renderer(std::vector<Flags> flags);
+
+            ~Renderer();
+
+            /// @brief Gets the maximum texture width.
+            int getMaxTextureWidth();
+            /// @brief Gets the maximum texture height.
+            int getMaxTextureHeight();
+
+            /// @brief Clears the current rendering target.
+            void clear();
+
+            /// @brief Renders the texture at the point @p dest.
+            void renderTexture(TextureInfo info, Point dest);
+            /// @brief Renders the texture at the point @p dest rotated by
+            /// @p angle degrees.
+            void renderTexture(TextureInfo info, Point dest, double angle,
+                    Point ref);
+            /// @brief Renders the texture at the point @p dest flipped as
+            /// specified in the vector @p flip.
+            void renderTexture(TextureInfo info, Point dest,
+                    std::vector<Flip> flip);
+            /// @brief Renders the texture at the point @p dest rotated by
+            /// @p angle degrees and flipped as specified in @p flip.
+            void renderTexture(TextureInfo info, Point dest, double angle,
+                    Point ref, std::vector<Flip> flip);
+
+            /// @brief Renders the texture inside the rectangle @p srcR
+            /// into @p dstR.
+            void renderTexture(TextureInfo info, Rect srcR, Rect dstR);
+            /// @brief Renders the texture inside the rectangle @p srcR
+            /// into @p dstR rotated by @p angle degrees.
+            void renderTexture(TextureInfo info, Rect srcR, Rect dstR,
+                    double angle, Point ref);
+            /// @brief Renders the texture inside the rectangle @p srcR
+            /// into @p dstR flipped as specified in @p flip.
+            void renderTexture(TextureInfo info, Rect srcR, Rect dstR,
+                    std::vector<Flip> flip);
+            /// @brief Renders the texture inside the rectangle @p srcR
+            /// into @p dstR rotated by @p angle degrees and flipped as
+            /// specified in @p flip.
+            void renderTexture(TextureInfo info, Rect srcR, Rect dstR,
+                    double angle, Point ref, std::vector<Flip> flip);
+
+            /// @brief Creates a texture from a surface.
             SDL_Texture *createTextureFromSurface(SDL_Surface *surface);
+
+        private:
+            void initRenderer(std::vector<Flags> flags);
+            void renderTextureImpl(TextureInfo info, Rect srcR, Rect dstR, 
+                    double angle, Point ref, std::vector<Flip> flip);
+
+            uint32_t toSDLFlag(Flags flag);
+            uint32_t toSDLFlags(std::vector<Flags> flags);
+            uint32_t toSDLRendererFlip(std::vector<Flip> flip);
     };
 }
 
--- lib/core/Renderer.cpp
+++ lib/core/Renderer.cpp
@@ -3,10 +3,71 @@
 #include "SDL2/SDL.h"
 
 #include "unk/core/Renderer.h"
-#include "unk/util/SDLException.h"
+#include "unk/core/Resources.h"
+#include "unk/utils/SDLException.h"
 
 unk::Renderer::Renderer() {
-    // create renderer from window
+    initRenderer({ Flags::ACCELERATED });
+}
+
+unk::Renderer::Renderer(std::vector<Flags> flags) {
+    initRenderer(flags);
+}
+
+unk::Renderer::~Renderer() {
+    SDL_DestroyRenderer(SDLRenderer);
+}
+
+int unk::Renderer::getMaxTextureWidth() {
+    return Info.max_texture_width;
+}
+
+int unk::Renderer::getMaxTextureHeight() {
+    return Info.max_texture_height;
+} 
+
+void unk::Renderer::renderTexture(TextureInfo info, Point dest) {
+    Rect srcR(0, 0, info.getWidth(), info.getHeight());
+    Rect dstR(dest.X, dest.Y, info.getWidth(), info.getHeight());
+    renderTextureImpl(info, srcR, dstR, 0.0, Point(0, 0), { Flip::NONE });
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Point dest, double angle, Point ref) {
+    Rect srcR(0, 0, info.getWidth(), info.getHeight());
+    Rect dstR(dest.X, dest.Y, info.getWidth(), info.getHeight());
+    renderTextureImpl(info, srcR, dstR, angle, ref, { Flip::NONE });
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Point dest, std::vector<Flip> flip) {
+    Rect srcR(0, 0, info.getWidth(), info.getHeight());
+    Rect dstR(dest.X, dest.Y, info.getWidth(), info.getHeight());
+    renderTextureImpl(info, srcR, dstR, 0.0, Point(0, 0), flip);
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Point dest, double angle, Point ref,
+        std::vector<Flip> flip) {
+    Rect srcR(0, 0, info.getWidth(), info.getHeight());
+    Rect dstR(dest.X, dest.Y, info.getWidth(), info.getHeight());
+    renderTextureImpl(info, srcR, dstR, angle, ref, flip);
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Rect srcR, Rect dstR) {
+    renderTextureImpl(info, srcR, dstR, 0.0, Point(0, 0), { Flip::NONE });
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Rect srcR, Rect dstR,
+        double angle, Point ref) {
+    renderTextureImpl(info, srcR, dstR, angle, ref, { Flip::NONE });
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Rect srcR, Rect dstR,
+        std::vector<Flip> flip) {
+    renderTextureImpl(info, srcR, dstR, 0.0, Point(0, 0), flip);
+}
+
+void unk::Renderer::renderTexture(TextureInfo info, Rect srcR, Rect dstR,
+        double angle, Point ref, std::vector<Flip> flip) {
+    renderTextureImpl(info, srcR, dstR, angle, ref, flip);
 }
 
 SDL_Texture *unk::Renderer::createTextureFromSurface(SDL_Surface *surface) {
@@ -15,4 +76,78 @@ SDL_Texture *unk::Renderer::createTextureFromSurface(SDL_Surface *surface) {
         throw SDLException();
 
     return texture;
+}
+
+void unk::Renderer::initRenderer(std::vector<Flags> flags) {
+    uint32_t sdlFlags = toSDLFlags(flags);
+    // create renderer from window
+
+    int ret = SDL_GetRendererInfo(SDLRenderer, &Info);
+
+    if (ret < 0)
+        throw SDLException();
+}
+
+void unk::Renderer::renderTextureImpl(TextureInfo info, Rect srcR, Rect dstR,
+        double angle, Point ref, std::vector<Flip> flip) {
+    SDL_Texture *texture = nullptr;
+
+    texture = Resources::getResources().getTexture(info);
+    if (!texture)
+        throw SDLException();
+
+    SDL_Rect src = srcR.toSDLRect();
+    SDL_Rect dest = dstR.toSDLRect();
+    SDL_Point rotationRef = ref.toSDLPoint();
+    SDL_RendererFlip flipOpt = static_cast<SDL_RendererFlip>(toSDLRendererFlip(flip));
+
+    int ret = SDL_RenderCopyEx(SDLRenderer, texture, &src, &dest, angle, &rotationRef, flipOpt);
+
+    if (ret < 0)
+        throw SDLException();
+}
+
+uint32_t unk::Renderer::toSDLFlag(Flags flag) {
+    switch (flag) {
+        case Flags::SOFTWARE:
+            return SDL_RENDERER_SOFTWARE;
+        case Flags::ACCELERATED:
+            return SDL_RENDERER_ACCELERATED;
+        case Flags::VSYNC:
+            return SDL_RENDERER_PRESENTVSYNC;
+        case Flags::TEXTURE:
+            return SDL_RENDERER_TARGETTEXTURE;
+        default:
+            throw SDLException("Invalid Renderer flag.");
+    }
+}
+
+uint32_t unk::Renderer::toSDLFlags(std::vector<Flags> flags) {
+    uint32_t sdlFlags = 0;
+
+    for (auto f : flags)
+        sdlFlags |= toSDLFlag(f);
+
+    return sdlFlags;
+}
+
+uint32_t unk::Renderer::toSDLRendererFlip(std::vector<Flip> flip) {
+    uint32_t sdlFlip = SDL_FLIP_NONE;
+
+    for (auto f : flip) {
+        switch (f) {
+            case Flip::HORIZONTAL:
+                sdlFlip |= SDL_FLIP_HORIZONTAL;
+                break;
+
+            case Flip::VERTICAL:
+                sdlFlip |= SDL_FLIP_VERTICAL;
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    return sdlFlip;
 }
