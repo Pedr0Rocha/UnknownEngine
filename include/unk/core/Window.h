0a315002e842230ebd93d014a93a9f88097968a5
pedr0rocha <pedr0_r@hotmail.com>
2016-09-26 22:50:03 -0300

    Indent using spaces

--- include/unk/core/Window.h
+++ include/unk/core/Window.h
@@ -10,71 +10,71 @@
 
 namespace unk {
 
-   
-	/**
-	* @brief Wraps the SDL equivalent (@c SDL_Window).
-	*
-	* @details Is responsible for every function implemented
-	* with the @c SDL_Window.
-	*/
-	class Window {
-		
-		public:
+    
+    /**
+    * @brief Wraps the SDL equivalent (@c SDL_Window).
+    *
+    * @details Is responsible for every function implemented
+    * with the @c SDL_Window.
+    */
+    class Window {
+        
+    public:
 
-			/**
-			 * @brief The @c SDL_WindowFlags.
-			*/
-			enum class Flags { 
-				OPENGL,			///< Window usable with OpenGL context.
-				BORDERLESS,		///< No window decoration.
-				RESIZABLE		///< Window can be resized.
-			};
+            /**
+             * @brief The @c SDL_WindowFlags.
+            */
+             enum class Flags { 
+                OPENGL,         ///< Window usable with OpenGL context.
+                BORDERLESS,     ///< No window decoration.
+                RESIZABLE       ///< Window can be resized.
+            };
 
-			std::string Title;
-			int X;
-			int Y;
-			int Width;
-			int Height;
-			SDL_Window* SDLWindow;
+            std::string Title;
+            int X;
+            int Y;
+            int Width;
+            int Height;
+            SDL_Window* SDLWindow;
 
 
-			/// @brief Creates a fully customized @c Window.
-			Window(std::string title, int x, int y, int width, int height, std::vector<Flags> flags);
-			/// @brief Creates a standard @c Window.
-			Window(std::string title);
-			/// @brief Creates a customized @c Window with width and height set
-			/// and standard flags.	 
-			Window(std::string title, int width, int height);
+            /// @brief Creates a fully customized @c Window.
+            Window(std::string title, int x, int y, int width, int height, std::vector<Flags> flags);
+            /// @brief Creates a standard @c Window.
+            Window(std::string title);
+            /// @brief Creates a customized @c Window with width and height set
+            /// and standard flags.  
+            Window(std::string title, int width, int height);
 
-			~Window();
+            ~Window();
 
-			/// @brief Creates a @c Renderer from a @c Window.
-			SDL_Renderer* createRenderer(int index, uint32_t flags);
+            /// @brief Creates a @c Renderer from a @c Window.
+            SDL_Renderer* createRenderer(int index, uint32_t flags);
 
-			/// @brief Use this function to set the current @c Window
-			/// to Fullscreen mode.
-			void setWindowFullscreen();
-			/// @brief Use this function to set the current @c Window
-			/// to Fake Fullscreen mode. It takes the size of the desktop.
-			void setWindowFakeFullscreen();
-			/// @brief Use this function to minimize the current @c Window.
-			void minimizeWindow();
-			/// @brief Use this function to maximize the current @c Window.
-			void maximizeWindow();
-			/// @brief Use this function to restore the current @c Window.
-			void restoreWindow();
+            /// @brief Use this function to set the current @c Window
+            /// to Fullscreen mode.
+            void setWindowFullscreen();
+            /// @brief Use this function to set the current @c Window
+            /// to Fake Fullscreen mode. It takes the size of the desktop.
+            void setWindowFakeFullscreen();
+            /// @brief Use this function to minimize the current @c Window.
+            void minimizeWindow();
+            /// @brief Use this function to maximize the current @c Window.
+            void maximizeWindow();
+            /// @brief Use this function to restore the current @c Window.
+            void restoreWindow();
 
-		private:
-			/// @brief Resposible for initializing and creating the @c SDL_Window
-			/// from the @c Window constructor.
-			void initWindow(std::string title, int x, int y, int width, int height, 
-				std::vector<Flags> flags);
-			
-			/// @brief Converts @c Window Flags to @c SDL_WindowFlags
-			uint32_t toSDLFlags(std::vector<Flags> flags);
-			/// @brief Converts a single @c Window Flag to its correspondent @c SDL_WindowFlag
-			uint32_t unk::Window::toSDLFlag(Flags flag);
-	};
-}
+        private:
+            /// @brief Resposible for initializing and creating the @c SDL_Window
+            /// from the @c Window constructor.
+            void initWindow(std::string title, int x, int y, int width, int height, 
+                std::vector<Flags> flags);
+            
+            /// @brief Converts @c Window Flags to @c SDL_WindowFlags
+            uint32_t toSDLFlags(std::vector<Flags> flags);
+            /// @brief Converts a single @c Window Flag to its correspondent @c SDL_WindowFlag
+            uint32_t unk::Window::toSDLFlag(Flags flag);
+        };
+    }
 
 #endif
--- lib/core/Window.cpp
+++ lib/core/Window.cpp
@@ -3,9 +3,9 @@
 #include "unk/core/Window.h"
 
 unk::Window::Window(std::string title, int x, int y, int width, int height, 
-	std::vector<Flags> flags) : Title(title), X(x), Y(y), Width(width), Height(height) {
-	
-	unk::Window::initWindow(title, x, y, width, height, flags);
+    std::vector<Flags> flags) : Title(title), X(x), Y(y), Width(width), Height(height) {
+    
+    unk::Window::initWindow(title, x, y, width, height, flags);
 }
 
 unk::Window::Window(std::string title) : Title(title) {
@@ -13,38 +13,38 @@ unk::Window::Window(std::string title) : Title(title) {
 }
 
 unk::Window::Window(std::string title, int width, int height) : Title(title), 
-	Width(width), Height(height) {
+    Width(width), Height(height) {
 
     unk::Window::initWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 
-    	width, height, {});
+        width, height, {});
 }
 
 unk::Window::~Window() {
-	SDL_DestroyWindow(SDLWindow);
+    SDL_DestroyWindow(SDLWindow);
 }
 
 void unk::Window::initWindow(std::string title, int x, int y, int width, int height, 
-	std::vector<Flags> flags) {
-	
-	SDL_Window *sdlWindow;
+    std::vector<Flags> flags) {
+    
+    SDL_Window *sdlWindow;
 
-	uint32_t sdlFlags = unk::Window::toSDLFlags(flags);
-	
-	sdlWindow = SDL_CreateWindow(title.c_str(), x, y, width, height, sdlFlags);
+    uint32_t sdlFlags = unk::Window::toSDLFlags(flags);
 
-	if (sdlWindow == nullptr) 
-		throw SDLException();
-	
-	SDLWindow = sdlWindow;
+    sdlWindow = SDL_CreateWindow(title.c_str(), x, y, width, height, sdlFlags);
+
+    if (sdlWindow == nullptr) 
+        throw SDLException();
+
+    SDLWindow = sdlWindow;
 }
 
 SDL_Renderer* unk::Window::createRenderer(int index, uint32_t flags) {
-	SDL_Renderer *renderer = SDL_CreateRenderer(SDLWindow, index, flags);
+    SDL_Renderer *renderer = SDL_CreateRenderer(SDLWindow, index, flags);
 
-	if (renderer == nullptr)
-		throw SDLException();
+    if (renderer == nullptr)
+        throw SDLException();
 
-	return renderer;
+    return renderer;
 }
 
 uint32_t unk::Window::toSDLFlag(Flags flag) {
@@ -61,36 +61,36 @@ uint32_t unk::Window::toSDLFlag(Flags flag) {
 }
 
 uint32_t unk::Window::toSDLFlags(std::vector<Flags> flags) {
-	uint32_t sdlFlags = 0;
-	
-	for (auto flag : flags) 
-		sdlFlags |= toSDLFlag(flag);
+    uint32_t sdlFlags = 0;
 
-	return sdlFlags;
+    for (auto flag : flags) 
+        sdlFlags |= toSDLFlag(flag);
+
+    return sdlFlags;
 }
 
 void unk::Window::setWindowFullscreen() {
-	int ret = SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN);
+    int ret = SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN);
 
-	if (ret < 0)
-		throw SDLException();
+    if (ret < 0)
+        throw SDLException();
 }
 
 void unk::Window::setWindowFakeFullscreen() {
-	int ret = SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
+    int ret = SDL_SetWindowFullscreen(SDLWindow, SDL_WINDOW_FULLSCREEN_DESKTOP);
 
-	if (ret < 0)
-		throw SDLException();
+    if (ret < 0)
+        throw SDLException();
 }
 
 void unk::Window::minimizeWindow() {
-	SDL_MinimizeWindow(SDLWindow);
+    SDL_MinimizeWindow(SDLWindow);
 }
 
 void unk::Window::maximizeWindow() {
-	SDL_MaximizeWindow(SDLWindow);
+    SDL_MaximizeWindow(SDLWindow);
 }
 
 void unk::Window::restoreWindow() {
-	SDL_RestoreWindow(SDLWindow);
+    SDL_RestoreWindow(SDLWindow);
 }
