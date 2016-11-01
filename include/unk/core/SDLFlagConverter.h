395966097a141d2cf27a7c1d986b4a0ef1603580
Yukio Siraichi <sir.yukio@gmail.com>
2016-10-31 22:26:49 -0200

    FlagConverter now compiles.
    It now has a constructor which accepts an initializer_list.

--- include/unk/core/SDLFlagConverter.h
+++ include/unk/core/SDLFlagConverter.h
@@ -3,7 +3,11 @@
 #ifndef UNK_SDL_FLAG_CONVERTER_H
 #define UNK_SDL_FLAG_CONVERTER_H
 
-#include "SDL2/SDL.H"
+#include "SDL2/SDL.h"
+
+#include "unk/core/Renderer.h"
+#include "unk/core/RenderOptions.h"
+#include "unk/utils/UnkExceptions.h"
 
 namespace unk {
     /**
@@ -12,35 +16,52 @@ namespace unk {
      * @details Converts any type of @a UnknownEngineFlag to @a SDL_Flag using bitwise OR 
      * if correctly mapped.
      */
-	template <class FlagType>
-	class SDLFlagConverter : public std::map<FlagType, uint32_t> {
+    template <class FlagType>
+        class SDLFlagConverter : public std::map<FlagType, uint32_t> {
+            public:
+                typedef std::map<FlagType, uint32_t> FlagMap;
 
-		/// @brief Returns an unsigned integer that corresponds to the bitwise OR operations
-		/// applied to the flag array.
-		uint32_t toSDLFlags(std::vector<FlagType> flags);
+                SDLFlagConverter<FlagType>();
+                SDLFlagConverter<FlagType>(std::initializer_list<typename FlagMap::value_type> ilist);
 
-		/// @brief Returns the @a SDLFlag that corresponds to the @p flag.
-		uint32_t toSDLFlag(FlagType flag);
-	};
+                /// @brief Returns an unsigned integer that corresponds to the bitwise OR operations
+                /// applied to the flag array.
+                uint32_t toSDLFlags(std::vector<FlagType> flags);
+
+                /// @brief Returns the @a SDLFlag that corresponds to the @p flag.
+                uint32_t toSDLFlag(FlagType flag);
+        };
 }
 
 /* Template Implementation */
 template <class FlagType>
+unk::SDLFlagConverter<FlagType>::SDLFlagConverter() {
+
+}
+
+template <class FlagType>
+unk::SDLFlagConverter<FlagType>::SDLFlagConverter(
+        std::initializer_list<typename FlagMap::value_type> ilist) : 
+    FlagMap(ilist) {
+
+}
+
+template <class FlagType>
 uint32_t unk::SDLFlagConverter<FlagType>::toSDLFlags(std::vector<FlagType> flags) {
-	uint32_t sdlFlags = 0;
+    uint32_t sdlFlags = 0;
 
-	for (auto flag : flags)
-		sdlFlags |= toSDLFlag(flag);
+    for (auto flag : flags)
+        sdlFlags |= toSDLFlag(flag);
 
-	return sdlFlags;
+    return sdlFlags;
 }
 
 template <class FlagType>
 uint32_t unk::SDLFlagConverter<FlagType>::toSDLFlag(FlagType flag) {
-	if (count(flag) > 0)
-		return (*this)[flag]
-	
-	throw KeyNotFoundException(std::to_string(flag));
+    if (this->count(flag) > 0)
+        return (*this)[flag];
+
+    throw KeyNotFoundException(std::to_string(static_cast<int>(flag)));
 }
 
 
